94abf56e22a9622a48bf878c691915714e03e644
王顺子 <wangshunzi@wangshunzideMacBook-Pro.local>
2015-07-20 15:38:07 +0800

    添加了一个牛逼的项目

---
+++ NewBee/NewBee/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewBee
+//
+//  Created by 王顺子 on 15/7/20.
+//  Copyright (c) 2015年 小码哥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NewBee/NewBee/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NewBee
+//
+//  Created by 王顺子 on 15/7/20.
+//  Copyright (c) 2015年 小码哥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
